4c2494e984ff6fed5216a6f9ac9605828557b461
ashviga <46813514+ashviga@users.noreply.github.com>
2019-02-12 06:34:50 -0800

    Create b103.c

---
+++ b103.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#define MAX 100
+int main()
+{
+	char str[MAX];	
+	int i;
+	printf("Enter a string: ");
+	scanf("%[^\n]s",str); 
+	for(i=0; str[i]!='\0'; i++)
+	{
+		if(i==0)
+		{
+			if((str[i]>='a' && str[i]<='z'))
+				str[i]=str[i]-32; 
+			continue; 
+		}
+		if(str[i]==' ')
+		{
+			++i;
+			if(str[i]>='a' && str[i]<='z')
+			{
+				str[i]=str[i]-32; 
+				continue; 
+			}
+		}
+		else
+		{
+	if(str[i]>='A' && str[i]<='Z')
+				str[i]=str[i]+32; 
+		}
+	}
+	printf("%s\n",str);
+	return 0;
+}
