c981b8ccd995b8119ed595197f30a6bda40120b1
ashviga <46813514+ashviga@users.noreply.github.com>
2019-02-13 07:42:57 -0800

    Create b20.c

---
+++ b20.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(void)
+{
+        int sum=0,i,n;
+      
+        printf("\nThe Value of N: ");
+        scanf("%d",&n);
+        for(i=1;i<=n;i++)
+        {
+            printf("\n5 * %d = %d",i,5*i);
+        }
+        
+        
+        return 0;
+}
+
