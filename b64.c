9ec8c8cb5822f0be4c12ffe80501e3dd950e5b8e
ashviga <46813514+ashviga@users.noreply.github.com>
2019-02-19 07:20:10 -0800

    Create b64.c

---
+++ b64.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,M,x;
+    scanf("%d %d",&a,&M);
+    x=a+M;
+    if(x%2==0)
+    {
+        printf("even");
+    }
+    else
+    {
+        printf("odd");
+    }
+return 0;
+}
