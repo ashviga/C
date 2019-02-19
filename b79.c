82829015a4f4a9324cec5407825ca97b3287a2cc
ashviga <46813514+ashviga@users.noreply.github.com>
2019-02-19 06:43:45 -0800

    Create b79.c

---
+++ b79.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+	int main(void)
+	{
+	int a,b,c,n;
+	printf("enter the two number");
+	scanf("%d %d",&a,&b);
+	c=a*b;
+	if(n=c*c)
+	{
+	    printf("yes");
+	}
+	else
+	{
+	  printf("no");
+	}
+	return 0;
+	}
