bd892e51b0c92fe11b762ae188b9eba95cf2e466
ashviga <46813514+ashviga@users.noreply.github.com>
2019-02-19 06:39:27 -0800

    Create b78.c

---
+++ b78.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+	int main()
+	{
+	  int a;
+	printf("enter the number");
+	scanf("%d",&a);
+	if(a%13==0)
+	{
+	  printf("yes");
+	}
+	else
+	{
+	  printf("no");
+	}
+	
+
+	return 0;
+	}
