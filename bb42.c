8a18287e50ebb38134db175e5eb7e576dfa0b2cf
ashviga <46813514+ashviga@users.noreply.github.com>
2019-02-20 21:25:57 +0530

    Rename b42.c to bb42.c

---
+++ bb42.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+   char a[1000],b[1000];
+   int i,j,k=0,s=0;
+   scanf("%s %s",a,b);
+   
+   for(i=0;a[i]!='\0';i++)
+   {
+       k++;
+   }
+   for(j=0;b[j]!='\0';j++)
+   {
+      s++;
+   }
+  if(k<l)
+  {
+  printf("%s",a);
+  }
+  else if(k>s)
+  {
+  printf("%s",a);
+  }
+  else
+  {
+  printf("%s",b);
+ 
+  return 0;
+}
