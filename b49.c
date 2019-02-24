e2a9748e2b1abe5bfe7cad4e26b9cc0cae915b00
ashviga <46813514+ashviga@users.noreply.github.com>
2019-02-24 09:38:21 +0530

    Create b49.c

---
+++ b49.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+	
+
+	int main()
+	{
+	  int a,b,n;
+	  printf("enter the numbers");
+	  scanf("%d",&a);
+	  scanf("%d",&b);
+	  n=a+b;
+	  
+	  printf("%d",n);
+	
+
+	return 0;
+	}
