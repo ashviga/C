ca69d7a8f3eff36b90c1b929a243182c08322080
ashviga <46813514+ashviga@users.noreply.github.com>
2019-02-24 11:34:50 +0530

    Create b60.c

---
+++ b60.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+	
+	int main()
+	{
+	    int num,s=0,i;
+	    scanf("%d",&num);
+	    for(i=1;i<=num;i++)
+	    {
+	        s=s+i;
+	    }
+	    printf("%d",s);
+	    return 0;
+	}
