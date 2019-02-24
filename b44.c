6698d20f16ec3a9074db142914e22f43691e7587
ashviga <46813514+ashviga@users.noreply.github.com>
2019-02-24 09:21:26 +0530

    Create b44.c

---
+++ b44.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main()
+{
+   int a;
+   scanf("%d",&a);
+   
+   if(a>=1 && a<=10)
+   {
+       printf("yes");
+   }
+   else
+   {
+       printf("no");
+   }
+ 
+  return 0;
+}
