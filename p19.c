5bb7abbe5f212cc17a52a4368ed4a85698322f3e
ashviga <46813514+ashviga@users.noreply.github.com>
2019-03-05 20:26:34 +0530

    Create p19.c

---
+++ p19.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+    int num,i,j,sum=0;
+scanf("%d",&num);
+
+if(num>=2&&num<=100000)
+{
+for(i=2;i<=num;i++)
+{
+  if(num%i==0)
+    {
+        
+        
+        for(j=2;j<=i/2;j++)
+        {
+            
+            
+            if(i%j==0)
+            {
+                
+                sum=1;
+                break;
+                
+            }
+        }
+if(sum==0)
+     printf("%d ",i);
+}}
+}
+    return 0;
+}
