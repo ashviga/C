931f0380fe6f9c81df7b332a99519388a7d55fab
ashviga <46813514+ashviga@users.noreply.github.com>
2019-03-08 20:08:53 +0530

    Create p67.c

---
+++ p67.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main()
+{
+int N,i,p=1;
+scanf("%d",&N);
+if(N>=1&&N<=25)
+
+{
+    
+    
+    
+    
+    for(i=1;i<=N;i++)
+    {
+        
+        p=p*i;
+    }
+    
+    
+    
+    printf("%d",p);
+    
+}
+
+    return 0;
+}
