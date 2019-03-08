038840512fc64e6e437ae3676c52b9a14eafe521
ashviga <46813514+ashviga@users.noreply.github.com>
2019-03-08 20:23:10 +0530

    Create p74.c

---
+++ p74.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+ int n,B,p=1,i;
+ scanf("%d %d",&n,&B);
+ 
+ 
+ 
+ 
+ for(i=1;i<=B;i++)
+ {
+     p=p*n;
+     }
+     printf("%d",p);
+     
+    return 0;
+}
